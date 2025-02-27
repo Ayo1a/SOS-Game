cea9b56ca3b80a607829a0728d8b7c73a4afe5c9
Yael Buchman <yael.tamir@gmail.com>
2025-02-27 22:21:25 +0200

    exe7 - solution

--- SOSGame.py
+++ SOSGame.py
@@ -1,94 +1,94 @@
+import numpy as np
+
 class SOSGame:
     PLAYER_1 = 1
     PLAYER_2 = 2
 
     def __init__(self):
-        self.board = [[' ' for _ in range(8)] for _ in range(8)]
+        """Initializes the game board and player settings."""
+        self.board = np.full((8, 8), ' ', dtype=str)
         self.current_player = SOSGame.PLAYER_1
         self.scores = {SOSGame.PLAYER_1: 0, SOSGame.PLAYER_2: 0}
         self.game_over = False
+        self.move_history = []
 
     def make_move(self, x, y, letter):
-        if self.board[x][y] != ' ' or letter not in ('S', 'O'):
+        """Executes a move and updates the score. The turn always switches after a move."""
+        if self.board[x, y] != ' ' or letter not in ('S', 'O'):
             raise ValueError("Invalid move")
-        self.board[x][y] = letter
-        points = self.check_sos(x, y)
-        self.scores[self.current_player] += points
-        if points == 0:
-            self.current_player = SOSGame.PLAYER_1 if self.current_player == SOSGame.PLAYER_2 else SOSGame.PLAYER_2
-        self.game_over = self.check_game_over()
 
-    def unmake_move(self, x, y):
-        self.board[x][y] = ' '
+        self.board[x, y] = letter
+        points_scored = self.check_sos(x, y)
+        self.scores[self.current_player] += points_scored
+        self.move_history.append((x, y, letter, self.current_player, points_scored))
+
+        if self.check_game_over():
+            self.game_over = True
+        else:
+            self.current_player = SOSGame.PLAYER_1 if self.current_player == SOSGame.PLAYER_2 else SOSGame.PLAYER_2
+
+    def unmake_move(self):
+        """Reverts the board to the previous state by undoing the last move."""
+        if not self.move_history:
+            return
+        x, y, letter, previous_player, points_scored = self.move_history.pop()
+        self.board[x, y] = ' '
+        self.scores[previous_player] -= points_scored
+        self.current_player = previous_player
         self.game_over = False
 
-    def clone(self):
-        clone = SOSGame()
-        clone.board = [row[:] for row in self.board]
-        clone.current_player = self.current_player
-        clone.scores = self.scores.copy()
-        clone.game_over = self.game_over
-        return clone
-
     def encode(self):
-        #TODO: changing 
-        flat_board_s = [1 if cell == 'S' else 0 for row in self.board for cell in row]
-        flat_board_o = [1 if cell == 'O' else 0 for row in self.board for cell in row]
-        # flat_board = [1 if cell == 'S' else 2 if cell == 'O' else 0 for row in self.board for cell in row]
-        return flat_board_s + flat_board_o + [self.current_player, self.scores[SOSGame.PLAYER_1], self.scores[SOSGame.PLAYER_2]]
+        """Encodes the game state as a one-hot tensor and extra information."""
+        s_map = np.array(self.board == 'S', dtype=np.float32)
+        o_map = np.array(self.board == 'O', dtype=np.float32)
+        empty_map = np.array(self.board == ' ', dtype=np.float32)
+        extra_info = np.array([self.current_player - 1, self.scores[1], self.scores[2], self.game_over], dtype=np.float32)
+        return np.stack([s_map, o_map, empty_map]), extra_info
 
     @staticmethod
-    def decode(action_index):
-        #TODO: changing 
-        x = action_index // 16
-        y = (action_index % 16) // 2
-        letter = 'S' if action_index % 2 == 0 else 'O'
-        return x, y, letter
+    def decode(index):
+        """Decodes an action index into a move (x, y, letter)."""
+        x, y, letter_id = index // 16, (index % 16) // 2, index % 2
+        return x, y, 'S' if letter_id == 0 else 'O'
 
     def legal_moves(self):
-        return [(x, y, letter) for x in range(8) for y in range(8) if self.board[x][y] == ' ' for letter in ('S', 'O')]
+        """Returns a list of all legal moves in the current board state."""
+        return [(x, y, letter) for x in range(8) for y in range(8) if self.board[x, y] == ' ' for letter in ('S', 'O')]
 
     def status(self):
-        if self.game_over:
-            return "Game Over", self.scores
-        return "Ongoing"
-
-    def check_sos(self, x, y):
-        points = 0
-        directions = [(1, 0), (0, 1), (1, 1), (1, -1)]
-        for dx, dy in directions:
-            for start in [-2, -1, 0]:
-                seq = [self.board[x + (start + i) * dx][y + (start + i) * dy] \
-                       for i in range(3) \
-                       if 0 <= x + (start + i) * dx < 8 and 0 <= y + (start + i) * dy < 8]
-                if seq == ['S', 'O', 'S']:
-                    points += 1
-        return points
+        """Returns the current game status: ongoing, winner, or draw."""
+        if not self.game_over:
+            return "ongoing"
+        if self.scores[1] > self.scores[2]:
+            return "Player 1 wins"
+        elif self.scores[2] > self.scores[1]:
+            return "Player 2 wins"
+        else:
+            return "Draw"
 
     def check_game_over(self):
-        return all(self.board[x][y] != ' ' for x in range(8) for y in range(8))
+        """Checks if the game is over by verifying if the board is full."""
+        return np.all(self.board != ' ')
 
-''' checked -  we don't really need it '''
-#TODO:
-''' unmake_move(x, y) does not restore the score '''
-# What is the problem?
-# In the current version, the function unmake_move(x, y) only deletes the letter from the board, but does not restore the score to its original state.
-# If we make a move where we created an "SOS" and received points, undoing the move will leave the score as if the move still existed!
+    def check_sos(self, x, y):
+        """Checks if a move creates an SOS sequence and returns the points scored."""
+        points = 0
+        directions = [(1, 0), (0, 1), (1, 1), (1, -1)]  # Horizontal, vertical, diagonal directions
 
-# Suggested improvement
-# We will add a data structure that will store the changes in the score, so that we can restore them when undoing a move.
-# We will add a stack for each player to track the changes in the score.
+        for dx, dy in directions:
+            # Case 1: The new letter is 'O' at the center (S O S)
+            if self.is_valid(x - dx, y - dy) and self.is_valid(x + dx, y + dy):
+                if self.board[x - dx, y - dy] == 'S' and self.board[x, y] == 'O' and self.board[x + dx, y + dy] == 'S':
+                    points += 1
 
-''' check_game_over() could be smarter '''
-# What's the problem?
-# Currently check_game_over() only ends the game when the entire board is full, but the game may be lost for both players before then.
+            # Case 2: The new letter is 'S' at the end of (S O S)
+            if self.is_valid(x - 2 * dx, y - 2 * dy) and self.is_valid(x - dx, y - dy):
+                if self.board[x - 2 * dx, y - 2 * dy] == 'S' and self.board[x - dx, y - dy] == 'O' and self.board[x, y] == 'S':
+                    points += 1
 
-# Suggested improvement
-# We will check if there are any moves left that could create an "SOS", not just if the board is full.
+        return points
 
-''' check_sos(x, y) could be more efficient '''
-# What's the problem?
-# Currently the function check_sos(x, y) rescans all directions on each move, even if certain options are known to be irrelevant.
-# This could be improved by pre-marking squares that could create an "SOS", and only scanning the area around the last move.
-# Suggested improvement
-# Instead of checking the entire board each time, we will only check the area closest to the last move.
+    @staticmethod
+    def is_valid(x, y):
+        """Checks if the given coordinates are within the board's boundaries."""
+        return 0 <= x < 8 and 0 <= y < 8
